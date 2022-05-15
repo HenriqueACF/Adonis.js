d5c6cc49a4c5e4c8163f3d442f8067e69ae210ec
henriqueacf <henrique.assis145@gmail.com>
2022-05-14 23:00:28 -0300

    init controller

--- adonis-db/database/migrations/1652470304148_users.ts
+++ adonis-db/database/migrations/1652470304148_users.ts
@@ -6,8 +6,9 @@ export default class Users extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('username')
-      table.string('email')
+      table.string('username').notNullable()
+      table.string('email').unique().notNullable()
+      table.boolean('is_admin').defaultTo(0)
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })

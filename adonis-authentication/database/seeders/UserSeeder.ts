ca1ad5177fe968441778e32d24e8793f72f14e95
henriqueacf <henrique.assis145@gmail.com>
2022-05-16 16:52:59 -0300

    feat:[Controller & Seeder] => criados o controller e seeder de user

---
+++ adonis-authentication/database/seeders/UserSeeder.ts
@@ -0,0 +1,12 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+
+export default class UserSeederSeeder extends BaseSeeder {
+  public async run () {
+    // Write your database queries inside the run method
+    await User.create({
+      email:'henrique@testeAdonis.com',
+      password:'adonis123'
+    })
+  }
+}
